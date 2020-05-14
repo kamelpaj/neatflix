6869112d6367d8421db6034195f54a95dfe87f0a
unknown <qurridutt@gmail.com>
2020-05-14 10:59:27 +0200

    can now connect to mongodb atlas

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config()
-
+const MongoClient = require('mongodb').MongoClient;
+const atlasPassword = process.env.DB_PASSWORD;
 const app = express();
+const uri = `mongodb+srv://atlasAdmin:${atlasPassword}@neatflix-iftiq.mongodb.net/test?retryWrites=true&w=majority`;
 
-const testKey = process.env.EXAMPLE_KEY;
-console.log(testKey);
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
  
 app.get('/', function (req, res) {
   res.send('Hello World')
